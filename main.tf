779ea593112d6043ac069b77d6afd033449fdfd9
Rajiv Shakya <rajivshaky@gmail.com>
2026-04-14 15:10:35 +0530

    user_data added with sample website

--- main.tf
+++ main.tf
@@ -14,15 +14,46 @@ provider "aws" {
 resource "aws_instance" "my_instance" {
   ami           = var.ami_id
   instance_type = var.instance_type
+  key_name = "aws-key"
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+  user_data = file("nginx.sh")
   metadata_options {
     http_tokens = "required"
   }
   root_block_device {
     encrypted = true
   }
+
   tags = {
-    Name : "Application-Server"
+    Name : "Web-Server"
   }
 }
 
+resource "aws_security_group" "web_sg" {
+  name        = "nginx-sg"
+  description = "Allow HTTP and SSH"
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # ⚠️ production me restrict karte hain
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
