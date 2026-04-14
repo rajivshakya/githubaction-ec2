93f436db847522b0be082e300cbc770f8690f970
Rajiv Shakya <rajivshaky@gmail.com>
2026-04-14 15:25:12 +0530

    tfsec fix1

--- main.tf
+++ main.tf
@@ -39,7 +39,7 @@ resource "aws_security_group" "web_sg" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # ✔ required
+    cidr_blocks = ["0.0.0.0/0"] #tfsec:ignore:aws-ec2-no-public-ingress-sgr
   }
 
   # 🔐 SSH (sirf tumhare IP se)
@@ -58,6 +58,6 @@ resource "aws_security_group" "web_sg" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"] #tfsec:ignore:aws-ec2-no-public-egress-sgr
   }
 }
